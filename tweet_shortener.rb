9d2a35be3d4ac444a756f289de543cf8b24dd701
Jordan Bacon <jmbacon92@gmail.com>
2019-06-23 01:42:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def dictionary
+  {
+"hello" => "hi",
+"to" => "2",
+"two" => "2",
+"too" => "2",
+"for" => "4",
+"four" => "4",
+'be' => "b",
+'you' => "b",
+"at" => "@",
+"and" => "&"
+}
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  tweet_array.map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word = word
+    end
+    tweet_array.join(" ")
+end
+end
