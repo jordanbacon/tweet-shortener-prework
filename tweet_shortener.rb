cbbce44af8b739f2b4828720e4589482414fe3cf
Jordan Bacon <jmbacon92@gmail.com>
2019-06-23 02:07:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -47,6 +47,7 @@ def shortened_tweet_truncator(tweet)
       if word_substituter(tweet).length > 140
         word_substituter(tweet)[0..137] + "..."
       else
-        tweet
+        word_substituter(tweet)
     end
+    
 end
